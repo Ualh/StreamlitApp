b32fecc772b1f927168650b71d07b41dffd5d83d
Ualh <105986401+Ualh@users.noreply.github.com>
2022-05-22 15:41:12 +0200

    Update v1.py

--- v1.py
+++ v1.py
@@ -45,7 +45,9 @@ model2 = load_model2()
 
 st.title("Customer Prediction App")
 st.markdown("This application is a Streamlit dashboard that can be used to *analyze* and **predict** customer income")
-
+st.markdown("The dataset used was found on" +
+            " [Kaggle](https://www.kaggle.com/datasets/imakash3011/customer-personality-analysis)\
+                .")
 #########################################################
 st.header(" ")
 st.header(" ")
